964cfe5c1df26119d0091c524aea321a39c8eb38
aayushi221 <aayushipandey221@gmail.com>
2022-03-22 00:30:16 +0530

    sidebar and navbar added

--- data.py
+++ data.py
@@ -3,19 +3,35 @@ import pandas as pd
 import numpy as np
 import time
 
-data = pd.read_csv("data//ETL-ELT Tool Accelerator_Finalized.xlsx - Detail Score.csv")
+# data = pd.read_csv("data//ETL-ELT Tool Accelerator_Finalized.xlsx - Detail Score.csv")
  
+# # st.dataframe(data)
+
+# st.dataframe(data, width=2000, height=900)
+
+# @st.cache
+# def ret_time():
+#     time.sleep(5)
+#     return time.time()
+
+# if st.checkbox("1"):
+#     st.write(ret_time())
+
+# if st.checkbox("2"):
+#     st.write(ret_time())
+
+data = pd.read_csv("data//ETL-ELT Tool Accelerator_Finalized.xlsx - Detail Score.csv")
+# dataframe = data.to_string()
 # st.dataframe(data)
+first = data[["Platform Features"]]
+print(first)
 
-st.dataframe(data, width=2000, height=900)
+# st.dataframe(dataframe[["Platform Features"]], width=2000, height=900)
+st.write(first)
 
-@st.cache
-def ret_time():
-    time.sleep(5)
-    return time.time()
-
-if st.checkbox("1"):
-    st.write(ret_time())
-
-if st.checkbox("2"):
-    st.write(ret_time())
+# @st.cache
+# def ret_time():
+#     time.sleep(5)
+#     return time.time()
+# loc = st.checkbox(first)
+# st.write(loc)
--- main.py
+++ main.py
@@ -1,22 +1,84 @@
 import streamlit as st
+import pandas as pd
 
-st.title("hello Streamlit")
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-st.header("header")
 
-st.subheader("subheader")
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
-st.text("textt ttexttttex ttttexttttexttttext tttexttttexttttext  tttexttttexttttexttttexttttexttttexttttexttttexttttexttttexttttexttttexttttexttt")
+
+
 
 st.markdown("""
-# h1 tag
-## h2 tag
-:moon: <br>
-:sunglasses:
- """, True)
+<!--Main Navigation-->
+<header>
+  <!-- Sidebar -->
+  <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-dark">
+    <div class="position-sticky">
+      <div class="list-group list-group-flush mx-3 mt-4">
+        <a
+          href="#"
+          class="list-group-item list-group-item-action py-2 ripple"
+          aria-current="true">
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Main dashboard</span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple active">
+          <i class="fas fa-chart-area fa-fw me-3"></i><span>Webiste traffic</span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+          ><i class="fas fa-lock fa-fw me-3"></i><span>Password</span></a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+          ><i class="fas fa-chart-line fa-fw me-3"></i><span>Analytics</span></a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple">
+          <i class="fas fa-chart-pie fa-fw me-3"></i><span>SEO</span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+          ><i class="fas fa-chart-bar fa-fw me-3"></i><span>Orders</span></a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+          ><i class="fas fa-globe fa-fw me-3"></i><span>International</span></a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i class="fas fa-building fa-fw me-3"></i><span>Partners</span></a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+          ><i class="fas fa-calendar fa-fw me-3"></i><span>Calendar</span></a
+        >
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+          ><i class="fas fa-users fa-fw me-3"></i><span>Users</span></a
+        >
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+          ><i class="fas fa-money-bill fa-fw me-3"></i><span>Sales</span></a
+        >
+      </div>
+    </div>
+  </nav>
+  <!-- Sidebar -->
 
-st.latex(r'''
-a +ar+ a r^2''')
+""", unsafe_allow_html=True)
 
-st.write("Yo", "123", "# new")
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark mx-10" >
+  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">ETL/ELT Utility Tool</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active mx-1rem" >
+        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="data.py" target="_blank">Your Choices</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://twitter.com/" target="_blank">Result</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
 
+data = pd.read_csv("data//ETL-ELT Tool Accelerator_Finalized.xlsx - Detail Score.csv")
+
+first = data[["Platform Features"]]
+print(first)
+
+st.write(first)
