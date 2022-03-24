3c1d9a92f14de3bd61fe6a9a0322a29a518df7ad
aayushi221 <aayushipandey221@gmail.com>
2022-03-24 10:31:43 +0530

    done

--- main.py
+++ main.py
@@ -14,38 +14,38 @@ st.markdown("""
 <!--Main Navigation-->
 <header>
   <!-- Sidebar -->
-  <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-dark">
-    <div class="position-sticky">
-      <div class="list-group list-group-flush mx-3 mt-4">
+  <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse">
+    <div class="position-sticky ">
+      <div class="list-group list-group-flush mx-3 mt-4 ">
         <a
           href="#"
-          class="list-group-item list-group-item-action py-2 ripple"
+          class="list-group-item list-group-item-action py-2 bg-dark ripple"
           aria-current="true">
           <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Main dashboard</span>
         </a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple active">
-          <i class="fas fa-chart-area fa-fw me-3"></i><span>Webiste traffic</span>
+        <a href="#" class="list-group-item list-group-item-action py-2 bg-dark ripple active">
+          <i class="fas fa-chart-area fa-fw me-3"></i><span>Source Connectors</span>
         </a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-          ><i class="fas fa-lock fa-fw me-3"></i><span>Password</span></a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-          ><i class="fas fa-chart-line fa-fw me-3"></i><span>Analytics</span></a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple">
-          <i class="fas fa-chart-pie fa-fw me-3"></i><span>SEO</span>
+        <a href="#" class="list-group-item list-group-item-action bg-dark py-2 ripple"
+          ><i class="fas fa-lock fa-fw me-3"></i><span>Category -2</span></a>
+        <a href="#" class="list-group-item list-group-item-action bg-dark py-2 ripple"
+          ><i class="fas fa-chart-line fa-fw me-3"></i><span>Category -3</span></a>
+        <a href="#" class="list-group-item list-group-item-action bg-dark py-2 ripple">
+          <i class="fas fa-chart-pie fa-fw me-3"></i><span>Category -4</span>
         </a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-          ><i class="fas fa-chart-bar fa-fw me-3"></i><span>Orders</span></a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-          ><i class="fas fa-globe fa-fw me-3"></i><span>International</span></a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i class="fas fa-building fa-fw me-3"></i><span>Partners</span></a>
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-          ><i class="fas fa-calendar fa-fw me-3"></i><span>Calendar</span></a
+        <a href="#" class="list-group-item list-group-item-action bg-dark py-2 ripple"
+          ><i class="fas fa-chart-bar fa-fw me-3"></i><span>Category -5</span></a>
+        <a href="#" class="list-group-item list-group-item-action bg-dark py-2 ripple"
+          ><i class="fas fa-globe fa-fw me-3"></i><span>Category -6</span></a>
+        <a href="#" class="list-group-item list-group-item-action bg-dark py-2 ripple"><i class="fas fa-building fa-fw me-3"></i><span>Category -7</span></a>
+        <a href="#" class="list-group-item list-group-item-action bg-dark py-2 ripple"
+          ><i class="fas fa-calendar fa-fw me-3"></i><span>Category -8</span></a
         >
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-          ><i class="fas fa-users fa-fw me-3"></i><span>Users</span></a
+        <a href="#" class="list-group-item list-group-item-action bg-dark py-2 ripple"
+          ><i class="fas fa-users fa-fw me-3"></i><span>Category -9</span></a
         >
-        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
-          ><i class="fas fa-money-bill fa-fw me-3"></i><span>Sales</span></a
+        <a href="#" class="list-group-item list-group-item-action bg-dark py-2 ripple"
+          ><i class="fas fa-money-bill fa-fw me-3"></i><span>Category -10</span></a
         >
       </div>
     </div>
@@ -55,18 +55,18 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark mx-10" >
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">ETL/ELT Utility Tool</a>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" >
+  <a class="navbar-brand " href="#" target="_blank">ETL/ELT Utility Tool</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active mx-1rem" >
+    <ul class="navbar-nav ml-20">
+      <li class="nav-item active ml-20" >
         <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="data.py" target="_blank">Your Choices</a>
+        <a class="nav-link" href="#" target="_blank">Your Choices</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="https://twitter.com/" target="_blank">Result</a>
